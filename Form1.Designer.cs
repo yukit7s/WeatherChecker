514d6a71d4c20b67dd58ce909000028be0153111
yuki <46600832+yukit7s@users.noreply.github.com>
2020-02-11 23:26:12 +0900

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace WeatherChecker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.areaBox = new System.Windows.Forms.ComboBox();
+            this.weatherIcon = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ファイルToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.終了ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.weatherIcon)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // areaBox
+            // 
+            this.areaBox.FormattingEnabled = true;
+            this.areaBox.Location = new System.Drawing.Point(62, 115);
+            this.areaBox.Name = "areaBox";
+            this.areaBox.Size = new System.Drawing.Size(226, 23);
+            this.areaBox.TabIndex = 0;
+            this.areaBox.SelectedIndexChanged += new System.EventHandler(this.CitySelected);
+            // 
+            // weatherIcon
+            // 
+            this.weatherIcon.Location = new System.Drawing.Point(354, 42);
+            this.weatherIcon.Name = "weatherIcon";
+            this.weatherIcon.Size = new System.Drawing.Size(386, 366);
+            this.weatherIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.weatherIcon.TabIndex = 1;
+            this.weatherIcon.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(108, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "都道府県を選択";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ファイルToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ファイルToolStripMenuItem
+            // 
+            this.ファイルToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.終了ToolStripMenuItem});
+            this.ファイルToolStripMenuItem.Name = "ファイルToolStripMenuItem";
+            this.ファイルToolStripMenuItem.Size = new System.Drawing.Size(65, 24);
+            this.ファイルToolStripMenuItem.Text = "ファイル";
+            // 
+            // 終了ToolStripMenuItem
+            // 
+            this.終了ToolStripMenuItem.Name = "終了ToolStripMenuItem";
+            this.終了ToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.終了ToolStripMenuItem.Text = "終了";
+            this.終了ToolStripMenuItem.Click += new System.EventHandler(this.ExitMenuClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.weatherIcon);
+            this.Controls.Add(this.areaBox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.weatherIcon)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox areaBox;
+        private System.Windows.Forms.PictureBox weatherIcon;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ファイルToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 終了ToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace WeatherChecker
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, string> cityNames;
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.cityNames = new Dictionary<string, string>();
+
+            this.cityNames.Add("東京", "3");
+            this.cityNames.Add("大阪府", "1");
+            this.cityNames.Add("愛知県", "2");
+            this.cityNames.Add("福岡県", "10");
+
+            foreach (KeyValuePair<string, string> data in this.cityNames)
+            {
+                areaBox.Items.Add(data.Key);
+            }
+        }
+
+        private void CitySelected(object sender, EventArgs e)
+        {
+            // 天気情報サービスにアクセスする
+            string cityCode = cityNames[areaBox.Text];
+            string url =
+                "http://and-idea.sbcr.jp/sp/90261/weatherCheck.php?city=" +
+                cityCode;
+            HttpClient client = new HttpClient();
+            string result = client.GetStringAsync(url).Result;
+
+            // 転記情報からアイコンのURLを取り出す
+            JObject jobj = JObject.Parse(result);
+            string todayweatherIcon = (string)((jobj["url"] as JValue).Value);
+            weatherIcon.ImageLocation = todayweatherIcon;
+        }
+
+        private void ExitMenuClicked(object sender, EventArgs e)
+        {
+            // フォームを閉じる
+            this.Close();
+        }
+    }
+}
